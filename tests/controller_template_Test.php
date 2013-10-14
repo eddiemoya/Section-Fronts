5bbd3a2aa43bc1840939ed29e1ac02ec2962a4c6
Eddie Moya <eddie.moya@gmail.com>
2013-10-14 16:09:18 -0500

    lots of unit tests, need more

--- tests/controller_template_Test.php
+++ tests/controller_template_Test.php
@@ -6,7 +6,7 @@ class controller_template_Test extends WP_UnitTestCase {
 	public function setUP()
 	{
 		parent::setUp();
-
+		wp_delete_post(1, true);
 		$term = wp_insert_term('test-category', 'category');
 		$this->term = get_term_by('slug', 'test-category', 'category');
 		$_REQUEST['tag_ID'] = $this->term->term_id;
@@ -34,4 +34,38 @@ class controller_template_Test extends WP_UnitTestCase {
 		$temp_cont->setup_node();
 		$this->assertAttributeInstanceOf('WP_Node_Factory', 'node', $temp_cont);
 	}
+
+	/**
+	 * This tests that we are able to create single posts will NULL post type and a colon for a title.
+	 *
+	 * @group wpnode
+	 * @group wpnode_class
+	 * @group bugfixes
+	 * @group colonbug
+	 *
+	 */
+	public function test_colon_bug(){
+		
+		if(isset($_REQUEST['tag_ID'])) 
+		{
+			unset($_REQUEST['tag_ID']);
+		}
+
+		$section = new Controller_Templates($this->term->taxonomy);
+		$section->setup_node();
+
+		$colon_post = get_page_by_title(': ', 'OBJECT', 'post');
+		$query = new WP_Query('post_type=post');
+
+		$this->assertAttributeEquals(0, 'post_count', $query, 'Test for Colon Bug failed. Wrong number of posts exist.');
+		$this->assertAttributeEmpty('posts', $query, 'Test for Colon Bug failed. Wrong number of posts exist.');
+		$this->assertNull($colon_post, 'Test for the Colon Bug failed. Found a post with a colon as the title');
+
+		$query = new WP_Query('post_type=post');
+
+		$this->assertAttributeEquals(0, 'post_count', $query, 'The control test for Colon Bug failed. Unable to delete all "colon" posts.');
+	}
+
+
+
 }
